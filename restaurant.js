95076bc76d636e1436444b50c8c970ee84caee94
Daniëlle Morssink <daniellemorssink@desktop-0i7nuhh.kea.local>
2020-08-28 11:46:42 +0200

    Added data js

---
+++ restaurant.js
@@ -0,0 +1,27 @@
+//fetch data//
+fetch("https://kea-alt-del.dk/t5/api/productlist")
+.then(function(response){
+    return response.json();
+})
+
+.then(function(data) {
+    console.log(data)
+})
+
+
+
+//whe we have our data
+
+//loop through products//
+
+
+//finding the template//
+
+
+//clone the template//
+
+
+//fill out the template//
+
+
+//append the template//
