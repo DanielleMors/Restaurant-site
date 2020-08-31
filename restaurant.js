306e588d17994db6a7d77fd5eba5b57ab189720f
Daniëlle Morssink <daniellemorssink@Danielles-MacBook-Pro.local>
2020-08-31 17:43:35 +0200

    discount

--- restaurant.js
+++ restaurant.js
@@ -1,69 +1,77 @@
 //fetch data//
 fetch("https://kea-alt-del.dk/t5/api/productlist")
-.then(function (response){
-    return response.json();
-})
+    .then(function (response) {
+        return response.json();
+    })
 
-.then(function (data) {
-    console.log(data)
-    dataReceived(data);
-})
+    .then(function (data) {
+        console.log(data)
+        dataReceived(data);
+    })
 //when we have our data
 
 //loop through products//
 function dataReceived(products) {
-products.forEach(showProduct)
+    products.forEach(showProduct)
 }
 
 //executed once for each product//
 function showProduct(myProduct) {
     console.log(myProduct)
 
-//finding the template//
-const temp = document.querySelector("#productTemplate").content;
+    //finding the template//
+    const temp = document.querySelector("#productTemplate").content;
 
-//clone the template// can be copy or called clone//
-const myCopy = temp.cloneNode(true);
+    //clone the template// can be copy or called clone//
+    const myCopy = temp.cloneNode(true);
+
+    if (myProduct.discount == 0) {
+        myCopy.querySelector(".price").textContent = myProduct.price + ", -";
+    } else {
+        let discount = myProduct.price - (myProduct.price * myProduct.discount * 0.01);
+        myCopy.querySelector(".price").textContent = myProduct.price + ",-";
+        myCopy.querySelector(".price-discount").classList.remove("hidden");
+        myCopy.querySelector(".price-discount").textContent = discount + ",-";
+    }
 
 
-//if (!myProduct.discount) {
-//    myCopy.querySelector(".discount").classList.add("hidden");
-//}   //ADD CLASS TO CSS "HIDDEN" //
+    //if (!myProduct.discount) {
+    //    myCopy.querySelector(".discount").classList.add("hidden");
+    //}   //ADD CLASS TO CSS "HIDDEN" //
 
-if (!myProduct.vegetarian) {
-    myCopy.querySelector(".vegetarian").remove();
+    if (!myProduct.vegetarian) {
+        myCopy.querySelector(".vegetarian").remove();
+    }
+
+    if (myProduct.soldout == false) {
+        myCopy.querySelector(".soldOut").classList.add("hidden");
+
+    }
+
+
+    //if (myProduct.soldout == true)  {
+    //    const p = document.querySelector("p");
+    //    p.textContent = "Sold Out";
+    //    p.classList.add("soldOut");
+    //    myCopy.querySelector(".soldOut").appendChild
+    //}
+
+    //    //setup classes for filtering
+    //    // 1.find the artticle
+    //    const.article = myCopy.querySelector("article");
+    //    // 2. add classes
+    //    if (myProduct.vegetarian) {
+    //        article.classList.add("vegetarian");
+    //    }
+
+    //fill out the template//
+    myCopy.querySelector(".dish-title").textContent = myProduct.name;
+    myCopy.querySelector(".short-description").textContent = myProduct.name;
+
+    //append the template//
+    document.querySelector(".starters").appendChild(myCopy);
 }
 
-if (myProduct.soldout == true) {
-myCopy.querySelector(".soldOut").classList.add("hidden");
-
-}
-
-
-//if (myProduct.soldout == true)  {
-//    const p = document.querySelector("p");
-//    p.textContent = "Sold Out";
-//    p.classList.add("soldOut");
-//    myCopy.querySelector(".soldOut").appendChild
-//}
-
-//    //setup classes for filtering
-//    // 1.find the artticle
-//    const.article = myCopy.querySelector("article");
-//    // 2. add classes
-//    if (myProduct.vegetarian) {
-//        article.classList.add("vegetarian");
-//    }
-//
-//
-
-//fill out the template//
-myCopy.querySelector(".dish-title").textContent = myProduct.name;
-myCopy.querySelector(".short-description").textContent = myProduct.name;
-
-//append the template//
-document.querySelector(".starters").appendChild(myCopy);
-}
 //
 //const veggieFilter = document.querySelector("#veggieFilter");
 //veggieFilter.addEventListener("click", veggieFilterClicked);
@@ -72,4 +80,3 @@ document.querySelector(".starters").appendChild(myCopy);
 //    const articles = document.querySelectorAll("article:not(.vegetarian)");
 ////    console.log(articles)
 //}
-
