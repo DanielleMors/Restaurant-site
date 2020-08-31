0c69c619e3c682ef9e74bbe2ef80a429125d6eb7
Daniëlle Morssink <daniellemorssink@Danielles-MacBook-Pro.local>
2020-08-31 15:17:30 +0200

    added if else

--- restaurant.js
+++ restaurant.js
@@ -1,27 +1,70 @@
 //fetch data//
 fetch("https://kea-alt-del.dk/t5/api/productlist")
-.then(function(response){
+.then(function (response){
     return response.json();
 })
 
-.then(function(data) {
+.then(function (data) {
     console.log(data)
+    dataReceived(data);
 })
-
-
-
-//whe we have our data
+//when we have our data
 
 //loop through products//
+function dataReceived(products) {
+products.forEach(showProduct)
+}
 
+//executed once for each product//
+function showProduct(myProduct) {
+    console.log(myProduct)
 
 //finding the template//
+const temp = document.querySelector("#productTemplate").content;
+
+//clone the template// can be copy or called clone//
+const myCopy = temp.cloneNode(true);
 
 
-//clone the template//
+//if (!myProduct.discount) {
+//    myCopy.querySelector(".discount").classList.add("hidden");
+//}   //ADD CLASS TO CSS "HIDDEN" //
 
+if (!myProduct.vegetarian) {
+    myCopy.querySelector(".vegetarian").remove();
+}
 
-//fill out the template//
+//
+//if (myProduct.soldout == true)  {
+//    const p = document.querySelector("p");
+//    p.textContent = "Sold Out";
+//    p.classList.add("soldOut");
+//    myCopy.querySelector("article").appendChild
+//}
+//
+//    //setup classes for filtering
+//    // 1.find the artticle
+//    const.article = myCopy.querySelector("article");
+//    // 2. add classes
+//    if (myProduct.vegetarian) {
+//        article.classList.add("vegetarian");
+//    }
+//
+//
+////fill out the template//
+//myCopy.querySelector(".dish-title").textContent = myProduct.name;
+//myCopy.querySelector(".short-description").textContent = myProduct.name;
+//
+////append the template//
+//document.querySelector(".starters").appendChild(myCopy);
+//
+//}
+//
+//const veggieFilter = document.querySelector("#veggieFilter");
+//veggieFilter.addEventListener("click", veggieFilterClicked);
+//
+//function veggieFilterClicked() {
+//    const articles = document.querySelectorAll("article:not(.vegetarian)");
+////    console.log(articles)
+//}
 
-
-//append the template//
